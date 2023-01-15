6785a7be8cdfa436f197fbe78312c805508ad97f
Sergey <windisel1251@gmail.com>
2023-01-15 18:04:14 +1200

    Исправлен вывод в програмах, согласно условиям задач

--- task_4/task_4.cs
+++ task_4/task_4.cs
@@ -12,8 +12,17 @@ Console.Write("Введите 3-е число: ");
 int number3 = int.Parse(Console.ReadLine());
 
 if (number1 > number2 && number1 > number3)
+    Console.WriteLine ($"{number1}, {number2}, {number3} -> {number1}");
+    else if (number2 > number3)
+    Console.WriteLine ($"{number1}, {number2}, {number3} -> {number2}");
+    else
+    Console.WriteLine ($"{number1}, {number2}, {number3} -> {number3}");
+
+/*
+if (number1 > number2 && number1 > number3)
     Console.WriteLine ("1-е число '" + number1 + "' больше");
     else if (number2 > number3)
     Console.WriteLine ("2-е число '" + number2 + "' больше");
     else
     Console.WriteLine ("3-е число '" + number3 + "' больше");
+*/
--- task_6/task_6.cs
+++ task_6/task_6.cs
@@ -12,6 +12,6 @@ int number = int.Parse(Console.ReadLine());
 int result = number % 2;
 
 if (result == 0)
-    Console.WriteLine ("Число '" + number + "' является четным");
+    Console.WriteLine (number + " -> да");
     else
-    Console.WriteLine ("Число '" + number + "' является не четным");
+    Console.WriteLine (number + " -> нет");
